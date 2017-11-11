c3f943160a7fbded15a7095c2154ae9e9a290879
HenriqueMorato <riquemorato@gmail.com>
2017-11-11 21:27:28 -0200

    jquery para form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,54 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require jquery_ujs
+
+$(function () {
+    $("#price").on("keyup blur", ".form-control", recalc);
+    
+    $('.quantity').keyup(function () {
+        calculateSum();        
+    });
+});
+
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function recalc() {
+    var total = 0;
+    $("#price").find('tr').each(function () {
+        var quantity = $(this).find('input.quantity').val();
+        var price = $(this).find('.price').text();
+        var unity = $(this).find('.unity').text();        
+        price = moneyToInt(price);
+
+        var priceTotal = (quantity * price);
+        total += isNumber(priceTotal) ? priceTotal : 0;
+        var quantityTotal = calculateQuantity(unity, quantity)
+        var real = numberToReal(priceTotal);
+        $(this).find('.total_price').text(real ? real : "");
+        $(this).find('.total_quantity').text(quantityTotal ? quantityTotal : "");   
+    });
+    var total_real = numberToReal(total);
+    $("#price_sum").text(total_real);      
+}
+
+function moneyToInt ( str )
+{
+    return parseFloat(str.replace("R$","").replace(".","").replace(",","."));
+}
+
+function numberToReal(number) {
+    var number = number.toFixed(2).split('.');
+    number[0] = "R$ " + number[0].split(/(?=(?:...)*$)/).join('.');
+    return number.join(',');
+}
+
+function calculateQuantity(str, qnt){
+    if(str=="Cento") {
+        return qnt * 100;
+    }
+    return qnt;
+}
