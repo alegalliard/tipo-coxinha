44ac0e06505a782de6b95ef82d3259b961130928
HenriqueMorato <riquemorato@gmail.com>
2017-11-13 14:30:24 -0200

    Implemented feature test [ci skip]

--- spec/features/cook_view_proposals_spec.rb
+++ spec/features/cook_view_proposals_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 feature 'View list of proposals' do
   scenario 'and has proposals' do
-    cooker = create(:user, name: 'Zezinho', email: 'test@test.com')
+    cook = create(:user, name: 'Zezinho', email: 'test@test.com')
     proposal = create(:proposal)
+
+    visit root_path
+    click_on 'Minhas Propostas'
+
+    expect(current_path).to match proposal_path(cook)
+    expect(page).to have_content proposal.delivery_date_time
+    expect(page).to have_content proposal.user.name
+    expect(page).to have_content proposal.total_price
   end
 end
