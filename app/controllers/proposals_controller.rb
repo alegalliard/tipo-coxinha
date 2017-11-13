bc68739f504291688e63559267558a8e6f1db3e1
Henrique e Luis Felipe <riquemorato@gmail.com>
2017-11-13 21:18:25 -0200

    user must be logged in to accept and has to be cook

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -25,7 +25,7 @@ class ProposalsController < ApplicationController
       @proposal.accepted!
       redirect_to request.referer
     else
-      redirect_to root_path
+      redirect_to root_path, error: 'Ação não autorizada'
     end
   end
 
